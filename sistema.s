27752cf8147a2dcc1802ec859223935d2005cb9a
Felipe Lunkes <felipemiguel_nery@hotmail.com>
2022-03-06 22:42:48 -0300

    1.0.3-beta1+200

--- sistema.s
+++ sistema.s
@@ -24,16 +24,16 @@ sistemaBase: ;; Dados de desenvolvimento OEM
 
 ;; Banner apenas para exibição. 
                                        
-.versaoAndromeda: db "1.0.2 [Thena]", 0 
+.versaoAndromeda: db "1.0.3 [Thena]", 0 
 
 ;; Para tratamento, usar opções abaixo, em inteiro e string
 
 .versao:     db 1
 .subversao:  db 0
-.revisao:    db 2
+.revisao:    db 3
 .lancamento: db 0
 .codenome:   db "Thena",0
-.montagem:   dw 108
+.montagem:   dw 200
 
 ;; Informações do pacote de atualizações de construção do Sistema
 
@@ -44,7 +44,7 @@ sistemaBase: ;; Dados de desenvolvimento OEM
 .ano:     db "2022", 0
 .mes:     db "03", 0
 .dia:     db "06", 0
-.hora:    db "22", 0
+.hora:    db "23", 0
 .minutos: db "00", 0
 
 ;; Forma de ler o código da construção:
@@ -61,4 +61,4 @@ sistemaBase: ;; Dados de desenvolvimento OEM
 ;; 1)   2)     3)    4)     5)
 ;; RC S1.10 , 1.5 , 195 , Aurora
 
-.build:  db "OFS1@.03,1.0.2,0108.Arishem,Thena", 0
+.build:  db "OFS1@.03,1.0.3,0200.Arishem,Thena", 0
