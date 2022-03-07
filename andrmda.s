25945f7689a835e7ce0172b4c404ebc5cd0f42f1
Felipe Lunkes <felipemiguel_nery@hotmail.com>
2022-03-06 21:30:22 -0300

    Remoção de andrmda.s
    Remoção da biblioteca descontinuada 'andrmda.s'

--- andrmda.s
+++
@@ -1,6 +0,0 @@
-;; Interface de transição
-;;
-;; Permite que aplicativos que incluam a antiga biblioteca "andrmda.s" possam
-;; ser compiladas e utilizem a biblioteca atual "hexagon.s" até serem atualizados.
-
-include "hexagon.s"
