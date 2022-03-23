f387d8796b381f905a2bf76b24a07ddb2f96de90
Felipe Lunkes <felipemiguel_nery@hotmail.com>
2022-03-22 23:17:50 -0300

    Build 213

--- sistema.s
+++ sistema.s
@@ -33,7 +33,7 @@ sistemaBase: ;; Dados de desenvolvimento OEM
 .revisao:    db 3
 .lancamento: db 0
 .codenome:   db "Thena",0
-.montagem:   dw 212
+.montagem:   dw 213
 
 ;; Informações do pacote de atualizações de construção do Sistema
 
@@ -43,9 +43,9 @@ sistemaBase: ;; Dados de desenvolvimento OEM
 
 .ano:     db "2022", 0
 .mes:     db "03", 0
-.dia:     db "21", 0
-.hora:    db "03", 0
-.minutos: db "00", 0
+.dia:     db "22", 0
+.hora:    db "23", 0
+.minutos: db "30", 0
 
 ;; Forma de ler o código da construção:
 ;; 1) Tipo de liberação: RC (Release Candidate), OF (Oficial) ou TB (Teste Beta)
@@ -61,4 +61,4 @@ sistemaBase: ;; Dados de desenvolvimento OEM
 ;; 1)   2)     3)    4)     5)
 ;; RC S1.10 , 1.5 , 195 , Aurora
 
-.build:  db "OFS3@.03,1.0.3,0212.Arishem,Thena", 0
+.build:  db "OFS3@.03,1.0.3,0213.Arishem,Thena", 0
